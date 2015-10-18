d2dfdc4748c2a0b68a4e03b2f5421aa4e5199f41
LeonSkop <Leonidas@Leonidass-MacBook-Pro.local>
2015-10-18 17:42:58 +1100

    Added minimum password length
    Made the validation for password length.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ActiveRecord::Base
   # Validations
-  validates_presence_of :email, :first_name, :last_name, :username
+  validates_presence_of :email, :first_name, :last_name, :username, :password
   validates :email, format: { with: /(.+)@(.+).[a-z]{2,4}/, message: "%{value} is not a valid email" }
   validates :username, :first_name, :last_name, format: { with: /\A[A-Za-z]{2,}\z/, message: "%{value} is not a valid input" }
   validates :username, :email, uniqueness: true
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => {:within => 8..40},
+                       :on => :create
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => {:within => 8..40},
+                       :on => :update        
 
   # Users can have interests
   acts_as_taggable_on :interests
