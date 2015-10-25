46fada3ab9a0ec0da6cfd6197700b30ff260f2d4
LeonSkop <Leonidas@Leonidass-MacBook-Pro.local>
2015-10-25 12:11:47 +1100

    Adding more comments
    I believe my commenting is done.

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,3 +1,8 @@
+# This class is a model to represent the source.
+# as it has multiple attributes it needs to be it's own model.
+
+# David Toulmin 638541
+
 class Source < ActiveRecord::Base
   has_many :articles
 end
--- app/models/your_mailer.rb
+++ app/models/your_mailer.rb
@@ -1,6 +1,22 @@
+# This class is responsible for generating an email that appears like the view
+# your_mailer depicts. It is a subclass of the Action Mailer class, and is using
+# the Mandrill mailer service under my API license.
+# Due to my account being free, there are several restictrions on the license
+# such as only 25 emails per hour and a total of 2000 free emails on the license.
+#
+# Author::    Leonidas Skopilianos (skl@student.unimelb.edu.au)
+# Copyright:: Copyright (c) 2015 Leonidas Skopilianos
+# Completed in Partial Completion of SWEN30006
+# At: The University Of Melbourne
+#
+#
 class YourMailer < ActionMailer::Base
   def email_name(user)
+    # Creates an instance of the user this email will be sent to.
     @user = user
+    # generates the mail with specific subjects, who it is from and to whom it is going.
+    # This will also render the view mentioned in the header of this file as the email
+    # contents that will be sent to the user.email.
     mail subject: 'Your News Feed!',
          to: user.email,
          from: 'NewsFeed@newsdigest.com'
